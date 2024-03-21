dc6ebe4ce06365919f993faf7a0f88b49ccd4a16
QuanQuan-CHO <12011619@mail.sustech.edu.cn>
2024-03-21 19:21:38 +0800

    Support multiple accounts

--- login.py
+++ login.py
@@ -6,39 +6,45 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 
 
-email, passwd = sys.argv[1:]
-#1.open browser
-options = webdriver.ChromeOptions()
-options.add_argument("--headless=new")
-driver = webdriver.Chrome(options=options)
-driver.set_window_size(1000, 720)
-driver.get("https://game.maj-soul.net/1/")
-print('Loading...')
-sleep(10)
+acccounts = int(len(sys.argv[1:])/2)
+print(f'Config {acccounts} accounts')
+for i in range(acccounts):
+    email = sys.argv[1+i]
+    passwd = sys.argv[1+i+acccounts]
+    print('----------------------------')
 
-#2.input email
-screen = driver.find_element(By.ID, 'layaCanvas')
-ActionChains(driver)\
-    .move_to_element_with_offset(screen, 250, -100)\
-    .click()\
-    .perform()
-driver.find_element(By.NAME, 'input').send_keys(email)
-print('Input email successfully')
+    #1.open browser
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless=new")
+    driver = webdriver.Chrome(options=options)
+    driver.set_window_size(1000, 720)
+    driver.get("https://game.maj-soul.net/1/")
+    print(f'Account {i+1} loading game...')
+    sleep(10)
 
-#3.input password
-ActionChains(driver)\
-    .move_to_element_with_offset(screen, 250, -50)\
-    .click()\
-    .perform()
-driver.find_element(By.NAME, 'input_password').send_keys(passwd)
-print('Input password successfully')
+    #2.input email
+    screen = driver.find_element(By.ID, 'layaCanvas')
+    ActionChains(driver)\
+        .move_to_element_with_offset(screen, 250, -100)\
+        .click()\
+        .perform()
+    driver.find_element(By.NAME, 'input').send_keys(email)
+    print('Input email successfully')
 
-#4.login
-ActionChains(driver)\
-    .move_to_element_with_offset(screen, 250, 50)\
-    .click()\
-    .perform()
-print('Entering game...')
-sleep(20) #loading...
-print('Login success')
-driver.quit()
+    #3.input password
+    ActionChains(driver)\
+        .move_to_element_with_offset(screen, 250, -50)\
+        .click()\
+        .perform()
+    driver.find_element(By.NAME, 'input_password').send_keys(passwd)
+    print('Input password successfully')
+
+    #4.login
+    ActionChains(driver)\
+        .move_to_element_with_offset(screen, 250, 50)\
+        .click()\
+        .perform()
+    print('Entering game...')
+    sleep(20) #loading...
+    print('Login success')
+    driver.quit()
